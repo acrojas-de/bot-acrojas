8a915edd2bfe99507c13d77253db35830ce69e46
Acrojas <TU_CORREO_DE_GITHUB>
2026-03-20 01:34:04 +0100

    auto deploy

--- engines/sniper_entry.py
+++ engines/sniper_entry.py
@@ -28,7 +28,7 @@ def bullish_rejection_candle(candle):
     return lower_wick > body * 1.5
 
 
-def sniper_entry(context, setup_5m, trap, last_candle):
+def sniper_entry(context, setup_5m, trap, last_candle, bias_4h=None, compression=None):
     near_zone = setup_5m["near_ema21"] or setup_5m["near_ema50"]
 
     if not near_zone:
