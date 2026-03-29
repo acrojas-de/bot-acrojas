af3bd1c84361752a56598c494d633603ef986e74
Acrojas <TU_CORREO_DE_GITHUB>
2026-03-29 21:52:17 +0200

    auto deploy

--- handlers/telegram/telegram_dispatcher.py
+++ handlers/telegram/telegram_dispatcher.py
@@ -1,5 +1,5 @@
 from handlers.telegram.ranking_handler import handle_ranking
-from handlers.telegram.history_handler import handle_history
+# from handlers.telegram.history_handler import handle_history
 from handlers.telegram.select_handler import handle_select
 # from handlers.telegram.trade_handler import handle_trades, handle_close
 
@@ -9,9 +9,9 @@ def dispatch_command(cmd, context):
         handle_ranking(context)
         return True
 
-    if cmd in ["/history", "history"]:
-        handle_history(context)
-        return True
+    # if cmd in ["/history", "history"]:
+    #     handle_history(context)
+    #     return True
 
     if cmd in ["/select", "select"]:
         result = handle_select(context)
